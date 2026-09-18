1baf61bf6d9af3215dc22cacfcc2b95f11fdcda3
Dang Libin <libin.dang@gmail.com>
2026-09-18 22:03:37 +0800

    [libc] Add host database and syslog declarations

--- libc/include/llvm-libc-macros/linux/netdb-macros.h
+++ libc/include/llvm-libc-macros/linux/netdb-macros.h
@@ -25,4 +25,25 @@
 #define EAI_SYSTEM -11
 #define EAI_OVERFLOW -12
 
+#define AI_PASSIVE 0x0001
+#define AI_CANONNAME 0x0002
+#define AI_NUMERICHOST 0x0004
+#define AI_V4MAPPED 0x0008
+#define AI_ALL 0x0010
+#define AI_ADDRCONFIG 0x0020
+#define AI_NUMERICSERV 0x0400
+
+#define NI_NUMERICHOST 1
+#define NI_NUMERICSERV 2
+#define NI_NOFQDN 4
+#define NI_NAMEREQD 8
+#define NI_DGRAM 16
+
+#define HOST_NOT_FOUND 1
+#define TRY_AGAIN 2
+#define NO_RECOVERY 3
+#define NO_DATA 4
+#define h_errno (*__h_errno_location())
+#define h_addr h_addr_list[0]
+
 #endif // LLVM_LIBC_MACROS_LINUX_NETDB_MACROS_H
---
+++ libc/include/llvm-libc-types/struct_hostent.h
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_TYPES_STRUCT_HOSTENT_H
+#define LLVM_LIBC_TYPES_STRUCT_HOSTENT_H
+
+struct hostent {
+  char *h_name;
+  char **h_aliases;
+  int h_addrtype;
+  int h_length;
+  char **h_addr_list;
+};
+
+#endif // LLVM_LIBC_TYPES_STRUCT_HOSTENT_H
