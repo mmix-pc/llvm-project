3a8fe7a4dfcc076ce80c7b47aa7b7fba1a887a50
Dang Libin <libin.dang@gmail.com>
2026-09-18 19:58:13 +0800

    [MMIX] Enable Linux fchdir in command utilities

--- libc/config/linux/mmix/CommandUtilities.cmake
+++ libc/config/linux/mmix/CommandUtilities.cmake
@@ -14,4 +14,5 @@ list(APPEND TARGET_LIBC_ENTRYPOINTS
   libc.src.sys.time.utimes
   libc.src.sys.time.settimeofday
   libc.src.unistd.chown
-  libc.src.unistd.lchown)
+  libc.src.unistd.lchown
+  libc.src.unistd.fchdir)
