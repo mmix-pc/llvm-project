e50f99f18939e94990ab2ead42a7be8dbc42d8f5
Dang Libin <libin.dang@gmail.com>
2026-09-18 19:58:04 +0800

    [MMIX] Enable Linux freopen in command utilities

--- libc/config/linux/mmix/CommandUtilities.cmake
+++ libc/config/linux/mmix/CommandUtilities.cmake
@@ -8,4 +8,5 @@ list(APPEND TARGET_LIBC_ENTRYPOINTS
   libc.src.stdlib.mkstemp
   libc.src.libgen.dirname
   libc.src.string.strverscmp
-  libc.src.stdio.fseeko)
+  libc.src.stdio.fseeko
+  libc.src.stdio.freopen)
